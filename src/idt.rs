4e20a5c1bb6f35e2f6bd9cab3eed39cbc4b489d9
Juuso Tuononen <jutuon@outlook.com>
2018-12-28 21:06:57 +0200

    Interrupt handler locations to IDT.

--- src/idt.rs
+++ src/idt.rs
@@ -2,9 +2,59 @@
 use x86::dtables::*;
 use x86::segmentation::*;
 
-extern {
-    fn interrupt_0();
-}
+use seq_macro::seq;
+
+seq!(N in 18..=255 {
+    extern "C" {
+        fn interrupt_0();
+        fn interrupt_1();
+        fn interrupt_2();
+        fn interrupt_3();
+        fn interrupt_4();
+        fn interrupt_5();
+        fn interrupt_6();
+        fn interrupt_7();
+        fn interrupt_with_error_8();
+        fn interrupt_9();
+        fn interrupt_with_error_10();
+        fn interrupt_with_error_11();
+        fn interrupt_with_error_12();
+        fn interrupt_with_error_13();
+        fn interrupt_with_error_14();
+        fn interrupt_15();
+        fn interrupt_16();
+        fn interrupt_with_error_17();
+        #(
+            fn interrupt_#N();
+        )*
+    }
+});
+
+seq!(N in 18..=255 {
+    const INTERRUPT_HANDLERS: [unsafe extern "C" fn (); 256] = [
+        interrupt_0,
+        interrupt_1,
+        interrupt_2,
+        interrupt_3,
+        interrupt_4,
+        interrupt_5,
+        interrupt_6,
+        interrupt_7,
+        interrupt_with_error_8,
+        interrupt_9,
+        interrupt_with_error_10,
+        interrupt_with_error_11,
+        interrupt_with_error_12,
+        interrupt_with_error_13,
+        interrupt_with_error_14,
+        interrupt_15,
+        interrupt_16,
+        interrupt_with_error_17,
+        #(
+            interrupt_#N,
+        )*
+    ];
+});
 
 pub struct IDT {
     entries: [Descriptor; 256],
@@ -18,8 +68,7 @@ impl IDT {
     pub fn load_idt() {
         unsafe {
             for (i, entry) in IDT_DATA.entries.iter_mut().enumerate() {
-                // TODO: functions
-                let function_position = interrupt_0 as u32;
+                let function_position = INTERRUPT_HANDLERS[i] as u32;
 
                 let descriptor = DescriptorBuilder::interrupt_descriptor(SegmentSelector::new(1, x86::Ring::Ring0), function_position)
                     .present()
@@ -59,14 +108,6 @@ extern "C" fn rust_interrupt_handler_with_error(
     interrupt_number: u32,
     error_code: u32
 ) {
-    use core::fmt::Write;
-
-    let text_buffer = unsafe {
-        crate::vga_text::VgaTextBuffer::new_unsafe()
-    };
-
-    let mut terminal = crate::terminal::Terminal::new(text_buffer);
-
-    writeln!(terminal, "Interrupt {}, error: {}",
-             interrupt_number, error_code);
+    panic!("Interrupt {}, error: {:#08x}",
+        interrupt_number, error_code);
 }
